dfc27f6e5656d97404e4ed0c0b8e591e881dba61
강민재 <38933647+phoi5675@users.noreply.github.com>
2022-07-13 19:37:57 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define MAX 9
+typedef enum {false, true} bool;
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+void func(int count){
+    if(count == M){
+        for (int i = 0; i < M; i++){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 1; i <= N; i++){
+        if (count > 0 && i < arr[count - 1]) {
+            continue;
+        }
+        if (count < N){
+            //visited[i] = true;
+            arr[count] = i;
+            func(count + 1);
+            //visited[i] = false;
+        }
+    }
+}
+int main(int argc, char const *argv[])
+{
+    scanf("%d%d", &N, &M);
+
+    func(0);
+
+    return 0;
+}

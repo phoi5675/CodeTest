43d6490f6f16c8707a59009268b1564ddfc7db7c
강민재 <38933647+phoi5675@users.noreply.github.com>
2023-12-12 20:16:15 +0900

    [Platinum V] Title: 백조의 호수, Time: 1344 ms, Memory: 257308 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -29,14 +29,17 @@ if __name__ == '__main__':
             for dy, dx in VEC:
                 ny, nx = dy + y, dx + x
                 if 0 <= ny < R and 0 <= nx < C:
-                    if not swan_visited[ny][nx]:
-                        swan_visited[ny][nx] = True
-                        if lake[ny][nx] != ICE:
+                    if not visited[ny][nx]:
+                        visited[ny][nx] = True
+                        # 호수인 지역은 오늘 탐색 가능하므로 현재 탐색 진행중인 Q에 삽입
+                        if lake[ny][nx] == LAKE:
                             Q.append((ny, nx))
+                        # 빙판인 지역은 오늘 얼음이 녹은 이후에 탐색 가능하므로 내일 탐색할 Q에 삽입
                         elif lake[ny][nx] == ICE:
                             bufQ.append((ny, nx))
                     if ny == s2[0] and nx == s2[1]:
                         reachable = True
+                        break
         return reachable, bufQ
 
 
@@ -44,8 +47,6 @@ if __name__ == '__main__':
     visited: List[List[bool]] = [[False] * C for _ in range(R)]
 
     elapsed: int = 0
-    swan_visited = deepcopy(visited)
-    ice_visited = deepcopy(visited)
     lake: List[List[str]] = []
     s1: List[int] = []
     s2: List[int] = []
@@ -66,11 +67,12 @@ if __name__ == '__main__':
                     s2 = [i, j]
                     line[j] = LAKE
             if line[j] == '.':
+                # 첫날은 모든 호수에 대해서 얼음이 인접했는지 확인해야 하므로 모든 물에 대해서 탐색
                 bufferQ.append((i, j))
         lake.append(line)
 
     buf_moveQ.append(s1)
-    swan_visited[s1[0]][s1[1]] = True
+    visited[s1[0]][s1[1]] = True
 
     while not is_reachable:
         # Calculate when swans can meet
